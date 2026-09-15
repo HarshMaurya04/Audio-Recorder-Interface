50656591d79341a4c9745506fd8f7455391a0596
Harsh Maurya <harshm8540@gmail.com>
2026-09-15 19:08:56 +0530

    simplify web audio recorder functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import StoryRecorder from "./components/StoryRecorder";
-import StudentReport from "./components/StudentReport";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<StoryRecorder />} />
-      <Route path="/report" element={<StudentReport />} />
-    </Routes>
-  );
+  return <StoryRecorder />;
 }
 
 export default App;
